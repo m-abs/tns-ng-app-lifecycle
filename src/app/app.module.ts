5070e6440af131c3d54111be7020afcd5d63f6fe
Morten Anton Bach Sjøgren <msn@nota.dk>
2019-02-12 14:21:05 +0100

    add trace

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { RouteReuseStrategy } from "@angular/router";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-
+import { routeReuseStrategyTraceCategory, routerTraceCategory } from "nativescript-angular/trace";
+import * as trace from "tns-core-modules/trace";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
-import { RouteReuseStrategy } from "@angular/router";
 import { CustomRouteReuseStrategy } from "./custom-route-reuse-strategy";
+import { ItemDetailComponent } from "./item/item-detail.component";
+import { ItemsComponent } from "./item/items.component";
+
+trace.enable();
+trace.addCategories([routerTraceCategory,routeReuseStrategyTraceCategory].join(','));
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
@@ -15,27 +19,18 @@ import { CustomRouteReuseStrategy } from "./custom-route-reuse-strategy";
 // import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
 
 @NgModule({
-    bootstrap: [
-        AppComponent
-    ],
-    imports: [
-        NativeScriptModule,
-        AppRoutingModule
-    ],
-    declarations: [
-        AppComponent,
-        ItemsComponent,
-        ItemDetailComponent
-    ],
-    providers: [{
-        provide: RouteReuseStrategy,
-        useClass: CustomRouteReuseStrategy
-    }],
-    schemas: [
-        NO_ERRORS_SCHEMA
-    ]
+  bootstrap: [AppComponent],
+  imports: [NativeScriptModule, AppRoutingModule],
+  declarations: [AppComponent, ItemsComponent, ItemDetailComponent],
+  providers: [
+    {
+      provide: RouteReuseStrategy,
+      useClass: CustomRouteReuseStrategy
+    }
+  ],
+  schemas: [NO_ERRORS_SCHEMA]
 })
 /*
 Pass your application module to the bootstrapModule function located in main.ts to start your app
 */
-export class AppModule { }
+export class AppModule {}
